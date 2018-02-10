5e3423ea834d5718b56a333bddfa8b98a031ce24
Leonard Andrew Spencer <thrawn369@gmail.com>
2018-02-10 14:25:33 +1030

    adding gizmo rendering to demo app

--- Source/project2D/Application2D.cpp
+++ Source/project2D/Application2D.cpp
@@ -2,6 +2,7 @@
 #include "Texture.h"
 #include "Font.h"
 #include "Input.h"
+#include "Gizmos.h"
 
 Application2D::Application2D() {
 
@@ -15,8 +16,7 @@ bool Application2D::startup() {
 	
 	m_2dRenderer = new aie::Renderer2D();
 
-	m_texture = new aie::Texture("./textures/numbered_grid.tga");
-	m_shipTexture = new aie::Texture("./textures/ship.png");
+	aie::Gizmos::create(255u, 255u, 65535u, 65535u);
 
 	m_font = new aie::Font("./font/consolas.ttf", 32);
 	
@@ -24,14 +24,16 @@ bool Application2D::startup() {
 	m_cameraY = 0;
 	m_timer = 0;
 
+	m_scene = new physics::PhysicsScene(0.05f);
+
+	//TODO put objects in scene
+
 	return true;
 }
 
 void Application2D::shutdown() {
 	
 	delete m_font;
-	delete m_texture;
-	delete m_shipTexture;
 	delete m_2dRenderer;
 }
 
@@ -58,6 +60,8 @@ void Application2D::update(float deltaTime) {
 	// exit the application
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
 		quit();
+
+	m_scene->update(deltaTime);
 }
 
 void Application2D::draw() {
@@ -71,30 +75,13 @@ void Application2D::draw() {
 	// begin drawing sprites
 	m_2dRenderer->begin();
 
-	// demonstrate animation
-	m_2dRenderer->setUVRect(int(m_timer) % 8 / 8.0f, 0, 1.f / 8, 1.f / 8);
-	m_2dRenderer->drawSprite(m_texture, 200, 200, 100, 100);
-
-	// demonstrate spinning sprite
-	m_2dRenderer->setUVRect(0,0,1,1);
-	m_2dRenderer->drawSprite(m_shipTexture, 600, 400, 0, 0, m_timer, 1);
-
-	// draw a thin line
-	m_2dRenderer->drawLine(300, 300, 600, 400, 2, 1);
-
-	// draw a moving purple circle
-	m_2dRenderer->setRenderColour(1, 0, 1, 1);
-	m_2dRenderer->drawCircle(sin(m_timer) * 100 + 600, 150, 50);
-
-	// draw a rotating red box
-	m_2dRenderer->setRenderColour(1, 0, 0, 1);
-	m_2dRenderer->drawBox(600, 500, 60, 20, m_timer);
-
-	// draw a slightly rotated sprite with no texture, coloured yellow
-	m_2dRenderer->setRenderColour(1, 1, 0, 1);
-	m_2dRenderer->drawSprite(nullptr, 400, 400, 50, 50, 3.14159f * 0.25f, 1);
+	// TODO extract stuff out into variables, make screen to world conversion functions
+	static float aspectRatio = 16 / 9.f;
+	aie::Gizmos::draw2D(glm::ortho<float>(-100, 100, -100 / aspectRatio, 100 / aspectRatio, -1.0f, 1.0f));
 	
-	// output some text, uses the last used colour
+	
+	// output some text in yellow
+	m_2dRenderer->setRenderColour(1, 1, 0, 1);
 	char fps[32];
 	sprintf_s(fps, 32, "FPS: %i", getFPS());
 	m_2dRenderer->drawText(m_font, fps, 0, 720 - 32);
--- Source/project2D/Application2D.h
+++ Source/project2D/Application2D.h
@@ -3,6 +3,8 @@
 #include "Application.h"
 #include "Renderer2D.h"
 
+#include "PhysicsScene.h"
+
 class Application2D : public aie::Application {
 public:
 
@@ -18,9 +20,9 @@ public:
 protected:
 
 	aie::Renderer2D*	m_2dRenderer;
-	aie::Texture*		m_texture;
-	aie::Texture*		m_shipTexture;
 	aie::Font*			m_font;
+
+	physics::PhysicsScene*	m_scene;
 
 	float m_cameraX, m_cameraY;
 	float m_timer;
